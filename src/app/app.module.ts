5f0493184dfbd532e4a7bf8f4438164ca96b249d
Nokk3r <nokk3r@mail.ru>
2021-01-14 20:58:41 +1100

    Add project 15

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TableWokersComponent } from './ui/table-wokers/table-wokers.component';
+import { AddformWorkerComponent } from './ui/addform-worker/addform-worker.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalComponent } from './ui/modal/modal.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TextMaskModule } from 'angular2-text-mask';
+import { MyFilterPipe } from './shared/pipes/my-filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableWokersComponent,
+    AddformWorkerComponent,
+    ModalComponent,
+    MyFilterPipe,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    TextMaskModule,
+    HttpClientModule,
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
